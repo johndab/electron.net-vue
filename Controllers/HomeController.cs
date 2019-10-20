58f1227e4a3ae978bd1bc6a5dcae9fb424974c23
Jan Dąbrowski <jan.dabrowski@ilabo.com.pl>
2019-10-20 13:13:40 +0100

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using electron.net_vue.Models;
+
+namespace electron.net_vue.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet("hello")]
+        public IActionResult Index()
+        {
+            return StatusCode(200, new { message = "Hello World!" });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+
+Create your front-end application:
+```bash
+cd wwwroot
+
+# Scaffold vue app
+vue create -n .
+
+# Install dependencies
+npm install
+```
+
+```
+electronize init
+
+electronize start
+```
